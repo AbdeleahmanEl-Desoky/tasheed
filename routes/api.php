56bdb7542765bb6d9e876c14b2804d14fb9e8e3d
AbdelrahmanEl-Desoky <abdelrhman@visualinnovate.com>
2024-09-01 02:38:55 +0300

    -

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\BlogController;
 use App\Http\Controllers\Api\HomeController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::get('storage-link', function () {
+    Artisan::call('storage:link');
+});
 Route::get('home', [HomeController::class,'index']);
 Route::get('blog', [BlogController::class,'index']);
 
