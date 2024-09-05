01aed20f4abb0e084872ffed420bc391789426be
AbdelrahmanEl-Desoky <abdelrhman@visualinnovate.com>
2024-09-06 02:54:56 +0300

    -

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +19,13 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('storage-link', function () {
     Artisan::call('storage:link');
+});
+
+
+Route::get('/clear-route-cache', function () {
+    Artisan::call('route:clear');
+    Artisan::call('optimize:clear');
+    return 'Route cache cleared!';
 });
 
 Route::get('home', [ApiController::class,'index']);
