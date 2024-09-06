b49a08d9679e79835343e478193990202c50e422
AbdelrahmanEl-Desoky <abdelrhman@visualinnovate.com>
2024-09-06 18:30:38 +0300

    -

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -116,10 +116,8 @@ class ApiController extends Controller
     {
         $singleProjectUnit = SingleProjectUnit::with(['unitFeatures','project'])->where('id',$id)->first();
 
-        // Decode the JSON field 'call_us' to a PHP array
-        if ($singleProjectUnit && $singleProjectUnit->data) {
+
             $singleProjectUnit->data = json_decode($singleProjectUnit->data, true);  // Decode JSON to array
-        }
 
         return response()->json([
             'project_unit'=>$singleProjectUnit,
